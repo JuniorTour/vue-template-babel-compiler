09c6f8fb7a8aaeff10ddc35bb27e511d9b3b025f
dandanDQ <danicashen@tencent.com>
2021-08-01 19:42:47 +0800

    fix: remove reduntant code.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import {renderCompiler} from "./renderCompiler"
 import {extendTemplateCompiler} from "./templateCompiler"
 
 // vue-es2015-compiler substitution:
-const transpile = renderCompiler
+const transpile = {}
 
 // enhance vue-template-compiler with babel:
 extendTemplateCompiler(transpile)
