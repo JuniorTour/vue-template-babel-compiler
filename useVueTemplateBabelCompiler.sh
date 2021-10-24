aac45927e6b3f9188add4d00eca5c30941af8686
juniortour <juniortour@qq.com>
2021-10-24 11:22:00 +0800

    chore: rm useVueTemplateBabelCompiler.sh

--- useVueTemplateBabelCompiler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mv node_modules/vue-template-es2015-compiler node_modules/vue-template-es2015-compiler-deprecated
-mv node_modules/vue-template-babel-compiler node_modules/vue-template-es2015-compiler
-echo 'Use vue-template-babel-compiler Success!'
